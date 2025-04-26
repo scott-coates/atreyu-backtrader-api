482a355c4c757f0fbd4dc750b2c736d66542fb70
Scott Coates <scoarescoare@gmail.com>
2025-04-25 23:44:59 -0500

    refactor: update package name and path in setup.py to ibapi_wrapper

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 _version_re = re.compile(r'__version__\s+=\s+(.*)')
 
-with open('atreyu_backtrader_api/__init__.py', 'rb') as f:
+with open('ibapi_wrapper/__init__.py', 'rb') as f:
     version = str(ast.literal_eval(_version_re.search(
         f.read().decode('utf-8')).group(1)))
 
@@ -17,9 +17,9 @@ if sys.version_info < (3,1):
     sys.exit("Only Python 3.1 and greater is supported")
 
 setup(
-    name='atreyu_backtrader_api',
+    name='ibapi_wrapper',
     version=version,
-    packages=['atreyu_backtrader_api'],
+    packages=['ibapi_wrapper'],
     url='https://github.com/atreyuxtrading/atreyu-backtrader-api',
     license='Simplified BSD license',
     author='Atreyu Trading',
